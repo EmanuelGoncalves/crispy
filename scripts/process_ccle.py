3737a6640764979ddb431d3d95b85e88bc74317c
Emanuel Goncalves <eg14@sanger.ac.uk>
2017-11-24 14:35:41 +0000

    CCLE crispr data processed

--- scripts/process_ccle.py
+++ scripts/process_ccle.py
@@ -17,20 +17,19 @@ essential = pd.read_csv('data/resources/curated_BAGEL_essential.csv', sep='\t')[
 nessential = pd.read_csv('data/resources/curated_BAGEL_nonEssential.csv', sep='\t')['gene'].rename('non-essential')
 
 # sgRNA library
-sgrna_lib = pd.read_csv('data/gdsc/crispr/KY_Library_v1.1_annotated.csv', index_col=0).dropna(subset=['GENES'])
+sgrna_lib = pd.read_csv('data/ccle/crispr_ceres/sgrnamapping.csv').dropna(subset=['Gene'])
 
 # Manifest
-manifest = pd.read_csv('data/gdsc/crispr/canapps_manifest.csv', index_col='header').query('to_use == True')
+manifest = pd.read_csv('data/ccle/crispr_ceres/replicatemap.csv', index_col=0)
+manifest = manifest.assign(plasmid=['pDNA_%d' % i for i in manifest['Batch']]).dropna()
 
 # Raw Counts
-counts = DataMatrix(pd.read_csv('data/gdsc/crispr/gdsc_crispr_rawcounts.csv', index_col=0))
+counts = DataMatrix(pd.read_csv('data/ccle/crispr_ceres/sgrna_raw_readcounts.csv', index_col=0))
 
 
-# - Processing
+# - Process
 # Build counts data-frame (discard sgRNAs with problems)
-qc_failed_sgrnas = [
-    'DHRSX_CCDS35195.1_ex1_X:2161152-2161175:+_3-1', 'DHRSX_CCDS35195.1_ex6_Y:2368915-2368938:+_3-3', 'DHRSX_CCDS35195.1_ex4_X:2326777-2326800:+_3-2', 'sgPOLR2K_1'
-]
+qc_failed_sgrnas = list(counts[counts.sum(1) < 30].index)
 counts = counts.drop(qc_failed_sgrnas, errors='ignore')
 
 # Add constant
@@ -42,8 +41,8 @@ counts = counts.divide(size_factors)
 
 # Calculate replicates correlation (pearson)
 corr_coefs = []
-for c in set(manifest['name']):
-    df = np.log2(counts[list(manifest.query("name == '%s'" % c).index)]).corr()
+for c in set(manifest['CellLine']):
+    df = np.log2(counts[list(manifest.query("CellLine == '%s'" % c).index)]).corr()
     corr_coefs.append(
         df.where(np.triu(np.ones(df.shape), k=1).astype(np.bool))
             .unstack()
@@ -65,23 +64,23 @@ fc = pd.DataFrame({c: np.log2(counts[c] / counts[manifest.loc[c, 'plasmid']]) fo
 fc = fc.loc[:, set(corr_coefs.query('pearson > .7')[['s1', 's2']].unstack())]
 
 # Average replicates
-fc = fc.groupby(manifest.loc[fc.columns, 'name'], axis=1).mean()
+fc = fc.groupby(manifest.loc[fc.columns, 'CellLine'], axis=1).mean()
 
 # Average gene level
-fc = fc.groupby(sgrna_lib.reindex(fc.index)['GENES']).mean().dropna()
+fc = fc.loc[sgrna_lib['Guide']].groupby(sgrna_lib['Gene'].values).mean().dropna()
 
 # Export
-fc.round(5).to_csv('data/crispr_gdsc_logfc.csv')
+fc.round(5).to_csv('data/crispr_ccle_logfc.csv')
 
 
 # - Benchmark
 # Essential/Non-essential genes AUC
 ax, stats_ess = plot_cumsum_auc(fc, essential, plot_mean=True, legend=False)
 plt.gcf().set_size_inches(3, 3)
-plt.savefig('reports/processing_gdsc_logfc_essential_auc.png', bbox_inches='tight', dpi=600)
+plt.savefig('reports/processing_ccle_logfc_essential_auc.png', bbox_inches='tight', dpi=600)
 plt.close('all')
 
 ax, stats_ness = plot_cumsum_auc(fc, nessential, plot_mean=True, legend=False)
 plt.gcf().set_size_inches(3, 3)
-plt.savefig('reports/processing_gdsc_logfc_nonessential_auc.png', bbox_inches='tight', dpi=600)
+plt.savefig('reports/processing_ccle_logfc_nonessential_auc.png', bbox_inches='tight', dpi=600)
 plt.close('all')
