58e763b937a4e6ba1f30b4d82d7b8e41fa1ddd6c
Emanuel Goncalves <eg14@sanger.ac.uk>
2018-01-02 16:30:00 +0000

    CRISPR/drug integration drug targets analysis.

--- scripts/drug/lmm_drug_crispr.py
+++ scripts/drug/lmm_drug_crispr.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # Copyright (C) 2018 Emanuel Goncalves
 
+import igraph
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import itertools as it
 import matplotlib.pyplot as plt
 from crispy import bipal_dbgd
-from limix.stats import qvalues, linear_kinship
-from limix.qtl import qtl_test_lmm
+from scipy.stats import pearsonr
 from scipy.stats.stats import rankdata
+from limix.stats import qvalues, linear_kinship
+from limix.qtl import qtl_test_lmm, qtl_test_lm
 from limix_core.util.preprocess import gaussianize
 
 
@@ -21,7 +24,7 @@ d_response = pd.read_csv('data/gdsc/drug_single/drug_ic50_merged_matrix.csv', in
 d_response.columns = d_response.columns.droplevel(0)
 
 # Samplesheet
-ss = pd.read_csv('data/gdsc/samplesheet.csv', index_col=0).dropna(subset=['Cancer Type'])
+ss = pd.read_csv('data/gdsc/samplesheet.csv', index_col=0).dropna(subset=['Cancer Type', 'Microsatellite'])
 
 # Essential genes
 essential = list(pd.read_csv('data/resources/curated_BAGEL_essential.csv', sep='\t')['gene'])
@@ -40,48 +43,82 @@ print('Samples: %d' % len(samples))
 # - Filter
 # Drug response
 d_response = d_response[d_response.count(1) > d_response.shape[1] * .85]
-d_response = d_response.loc[(d_response < d_response.mean().mean()).sum(1) >= 3]
+d_response = d_response.loc[(d_response < d_response.mean().mean()).sum(1) >= 5]
 
 # CRISPR
 crispr = crispr[(crispr < crispr.loc[essential].mean().mean()).sum(1) < (crispr.shape[1] * .5)]
-crispr = crispr[(crispr.abs() >= 1.5).sum(1) >= 3]
-crispr = crispr.drop(essential, axis=0, errors='ignore')
+crispr = crispr[(crispr.abs() >= 2).sum(1) >= 5]
 
 
 # - lmm: drug ~ crispr + tissue
 print('CRISPR genes: %d, Drug: %d' % (len(set(crispr.index)), len(set(d_response.index))))
 
-# Linear regressions
-lmm_cd = pd.DataFrame()
+# Build data-frames
+ys = d_response[samples].T
+ys = ys.fillna(ys.mean())
 
-# d = (1047, 'Nutlin-3a (-)', 'RS')
-for d in d_response.index:
-    # Discard samples with NaN for drug measurements
-    y = d_response.loc[[d], samples].T.dropna()
-    x = crispr[y.index].T
+xs = crispr[ys.index].T
+xs = xs.subtract(xs.mean())
+# xs = pd.DataFrame(gaussianize(xs.values), index=xs.index, columns=xs.columns)
 
-    # Random effects
-    covs = pd.get_dummies(ss.loc[samples, ['Cancer Type']]).loc[y.index].astype(float)
+k = kinship.loc[ys.index, ys.index]
 
-    # Normalise
-    x = pd.DataFrame(gaussianize(x.values), index=x.index, columns=x.columns)
+covs = pd.get_dummies(ss.loc[ys.index, ['Cancer Type', 'Microsatellite']]).astype(float)
 
-    # Linear regression
-    lmm = qtl_test_lmm(pheno=y.values, snps=x.loc[y.index].values, K=kinship.loc[y.index, y.index].values)
+# Linear regression
+lmm = qtl_test_lmm(pheno=ys.values, snps=xs.values, K=k.values, covs=covs.values)
 
-    # Extract solutions
-    res = pd.DataFrame({
-        'drug_id': d[0], 'drug_name': d[1], 'drug_screen': d[2],
-        'crispr': x.columns,
-        'beta': lmm.getBetaSNP().ravel(), 'pvalue': lmm.getPv().ravel(),
-        'rank_beta': rankdata(lmm.getBetaSNP().ravel()), 'rank_pval': rankdata(lmm.getPv().ravel()),
-        'len': len(y.index)
-    })
-    lmm_cd = lmm_cd.append(res)
+# Adjust p-values
+qvalue = qvalues(lmm.getPv())
 
-lmm_cd['qvalue'] = qvalues(lmm_cd['pvalue'].values)
+# Extract solutions
+drugs, genes = zip(*list(it.product(ys.columns, xs.columns)))
+drug_id, drug_name, drug_screen = zip(*drugs)
+
+lmm_cd = pd.DataFrame({
+    'drug_id': drug_id, 'drug_name': drug_name, 'drug_screen': drug_screen, 'crispr': genes,
+    'beta': lmm.getBetaSNP().ravel(),
+    'pvalue': lmm.getPv().ravel(),
+    'qvalue': qvalue.ravel()
+})
+
+
+# - PPI
+# Drug targets manual curated list
+ds = pd.read_csv('data/gdsc/drug_samplesheet.csv', index_col=0)
+
+d_targets = ds['Target Curated'].dropna().to_dict()
+d_targets = {k: {t.strip() for t in d_targets[k].split(';')} for k in d_targets}
+
+# Biogrid
+net_i = igraph.Graph.Read_Pickle('data/resources/igraph_string.pickle')
+net_i_genes = set(net_i.vs['name'])
+net_i_crispr = net_i_genes.intersection(set(lmm_cd['crispr']))
+
+# Distance to drug target
+d_targets_dist = pd.DataFrame({
+    d: np.min(net_i.shortest_paths(source=d_targets[d].intersection(net_i_genes), target=net_i_crispr), axis=0) for d in d_targets if len(d_targets[d].intersection(net_i_genes)) > 0
+}, index=net_i_crispr)
+
+
+# Annotate associations
+def drug_target_distance(d, t):
+    dst = np.nan
+
+    if (d in d_targets) and (t in d_targets[d]):
+        dst = 0
+
+    elif (d in d_targets_dist.columns) and (t in d_targets_dist.index):
+        dst = d_targets_dist.loc[t, d]
+
+    return dst
+
+
+lmm_cd = lmm_cd.assign(target=[drug_target_distance(d, t) for d, t in lmm_cd[['drug_id', 'crispr']].values])
+
+
+# - Export associations
 lmm_cd.sort_values('qvalue').to_csv('data/drug/lmm_drug_crispr.csv', index=False)
-# lmm_cd = pd.read_csv('data/lmm_crispr_drug_associations.csv')
 print(lmm_cd[lmm_cd['qvalue'] < 0.05].sort_values('qvalue'))
 
 
@@ -92,12 +129,17 @@ plot_df['log10_qvalue'] = -np.log10(plot_df['qvalue'])
 
 pal = dict(zip(*(['*', '-'], sns.light_palette(bipal_dbgd[0], 3, reverse=True).as_hex()[:-1])))
 
-g = sns.lmplot(
-    x='beta', y='log10_qvalue', data=plot_df, hue='signif', fit_reg=False, palette=pal, legend=False,
-    scatter_kws={'edgecolor': 'w', 'lw': .1, 's': 10, 'alpha': 0.4}
+# Scatter
+for i in ['*', '-']:
+    plt.scatter(
+        plot_df.query("(target != 0) & (signif == '%s')" % i)['beta'], plot_df.query("(target != 0) & (signif == '%s')" % i)['log10_qvalue'],
+        edgecolor='white', lw=.1, s=5, alpha=.5, c=pal[i]
+    )
+
+plt.scatter(
+    plot_df.query('target == 0')['beta'], plot_df.query('target == 0')['log10_qvalue'],
+    edgecolor='white', lw=.1, s=5, alpha=.5, c=bipal_dbgd[1]
 )
-g.axes[0, 0].set_ylim(0,)
-g.despine(right=False, top=False)
 
 # Add FDR threshold lines
 plt.text(plt.xlim()[0]*.98, -np.log10(.05) * 1.01, 'FDR 5%', ha='left', color=pal['*'], alpha=0.65, fontsize=5)
@@ -115,3 +157,92 @@ plt.ylabel('q-value (-log10)', fontsize=8, fontname='sans-serif')
 plt.gcf().set_size_inches(2., 4.)
 plt.savefig('reports/drug/lmm_crispr_volcano.png', bbox_inches='tight', dpi=300)
 plt.close('all')
+
+
+# - Corrplot
+idx = 239896
+
+d_id, d_name, d_screen, gene = lmm_cd.loc[idx, ['drug_id', 'drug_name', 'drug_screen', 'crispr']].values
+
+plot_df = pd.DataFrame({
+    'drug': d_response.loc[(d_id, d_name, d_screen), samples],
+    'crispr': crispr.loc[gene, samples],
+}).dropna()
+plot_df = pd.concat([plot_df, ss.loc[plot_df.index, ['Cancer Type', 'Microsatellite']]], axis=1)
+
+
+def marginal_distplot(a, vertical=False, **kws):
+    x = plot_df['drug'] if vertical else plot_df['crispr']
+    ax = sns.distplot(x, vertical=vertical, **kws)
+    ax.set_ylabel('')
+    ax.set_xlabel('')
+    ax.set_ylim((min(x) * 1.05, max(x) * 1.05)) if vertical else ax.set_xlim((min(x) * 1.05, max(x) * 1.05))
+
+
+sns.set(
+    style='ticks', context='paper', font_scale=.75,
+    rc={'axes.linewidth': .3, 'xtick.major.width': .3, 'ytick.major.width': .3, 'xtick.major.size': 2.5, 'ytick.major.size': 2.5}
+)
+g = sns.JointGrid('crispr', 'drug', plot_df, space=0, ratio=8)
+
+sns.regplot(
+    x='crispr', y='drug', data=plot_df, ax=g.ax_joint,
+    color=bipal_dbgd[0], truncate=True, fit_reg=True,
+    scatter_kws={'s': 10, 'edgecolor': 'w', 'linewidth': .3, 'alpha': .8}, line_kws={'linewidth': .5}
+)
+
+g.plot_marginals(marginal_distplot, color=bipal_dbgd[0], kde=False)
+
+g.annotate(pearsonr, template='R={val:.2g}, p={p:.1e}')
+
+g.ax_joint.axhline(0, ls='-', lw=0.1, c=bipal_dbgd[0])
+g.ax_joint.axvline(0, ls='-', lw=0.1, c=bipal_dbgd[0])
+
+g.set_axis_labels('%s (fold-change)' % gene, '%s (ln IC50)' % d_name)
+
+plt.gcf().set_size_inches(2, 2)
+plt.savefig('reports/drug/crispr_drug_corrplot.png', bbox_inches='tight', dpi=600)
+plt.close('all')
+
+
+# - Drug target Histogram
+sns.distplot(lmm_cd.query('target != 0')['beta'], label='All', color=bipal_dbgd[0], kde_kws={'lw': 1.})
+sns.distplot(lmm_cd.query('target == 0')['beta'], label='Target', color=bipal_dbgd[1], kde_kws={'lw': 1.})
+
+plt.xlabel('Drug ~ CRISPR beta')
+plt.ylabel('Density')
+plt.legend(loc='upper left')
+
+plt.gcf().set_size_inches(3, 2)
+plt.savefig('reports/drug/drug_target_hist.png', bbox_inches='tight', dpi=600)
+plt.close('all')
+
+
+# - Drug target Cumulative distributions
+ax = plt.gca()
+
+_, _, patches = ax.hist(
+    lmm_cd.query('target != 0')['beta'], lmm_cd.query('target != 0').shape[0],
+    cumulative=True, normed=1, histtype='step', label='All', color=bipal_dbgd[0], lw=1.
+)
+patches[0].set_xy(patches[0].get_xy()[:-1])
+
+_, _, patches = ax.hist(
+    lmm_cd.query('target == 0')['beta'], lmm_cd.query('target == 0').shape[0],
+    cumulative=True, normed=1, histtype='step', label='Target', color=bipal_dbgd[1], lw=1.
+)
+patches[0].set_xy(patches[0].get_xy()[:-1])
+
+ax.axvline(0, lw=.1, c=bipal_dbgd[0], alpha=.5, ls='-')
+ax.axhline(0, lw=.1, c=bipal_dbgd[0], alpha=.5, ls='-')
+
+# ax.set_title('Copy-number correlation with CRISPR bias')
+ax.set_xlabel('Drug ~ CRISPR beta')
+ax.set_ylabel('Cumulative distribution')
+
+ax.legend(loc='upper left')
+
+plt.gcf().set_size_inches(2.5, 2)
+plt.savefig('reports/drug/drug_target_cum_dist.png', bbox_inches='tight', dpi=600)
+plt.close('all')
+
