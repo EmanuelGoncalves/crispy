3e0fca714eec72df02700f7c71998f147f0ec67d
Emanuel Goncalves <eg14@sanger.ac.uk>
2018-01-15 17:12:29 +0000

    SV brass updated

--- scripts/crispy/wgs_brass_cnv_ratio.py
+++ scripts/crispy/wgs_brass_cnv_ratio.py
@@ -13,18 +13,10 @@ from sklearn.metrics import roc_curve, auc
 
 # - Import
 # Brass
-brass_files = filter(lambda x: x.endswith('.brass.annot.bedpe'), os.listdir('data/gdsc/wgs/brass_bedpe/'))
-brass_df = pd.concat([pd.read_csv('data/gdsc/wgs/brass_bedpe/%s' % f, skiprows=110, sep='\t') for f in brass_files]).reset_index(drop=True)
-brass_df.to_csv('data/gdsc/wgs/wgs_brass_bedpe.csv')
+brass_df = pd.read_csv('data/gdsc/wgs/wgs_brass_bedpe.csv')
 
 # Gene copy-number ratio
 cnv_ratios = pd.read_csv('data/crispy_copy_number_ratio.csv', index_col=0).replace(-1, np.nan)
-
-# Copy-number absolute counts
-cnv_abs = pd.read_csv('data/crispy_gene_copy_number.csv', index_col=0).replace(-1, np.nan)
-
-# GDSC CRISPR
-c_gdsc_fc = pd.read_csv('data/crispr_gdsc_logfc.csv', index_col=0)
 
 # GDSC CRISPR mean bias
 c_gdsc = pd.DataFrame({
@@ -42,14 +34,15 @@ print('Samples: ', len(samples), 'Genes: ', len(genes))
 
 # -
 sv_ratio = brass_df.loc[list(map(lambda x: len(x.split(',')) == 1, brass_df['sample']))]
+sv_ratio = sv_ratio[~sv_ratio['# chr1'].isin(['X', 'Y'])]
+sv_ratio = sv_ratio.query("copynumber_flag == 1 & assembly_score != '_'")
 
 sv_ratio = pd.melt(sv_ratio, id_vars=['sample', 'svclass'], value_vars=['gene1', 'gene2']).query("value != '_'")
 sv_ratio = sv_ratio[sv_ratio['value'].isin(genes) & sv_ratio['sample'].isin(samples)]
 
-sv_ratio = sv_ratio.assign(ratio=[cnv_abs.loc[g, s] for g, s in sv_ratio[['value', 'sample']].values]).dropna().sort_values('ratio')
+sv_ratio = sv_ratio.assign(ratio=[cnv_ratios.loc[g, s] for g, s in sv_ratio[['value', 'sample']].values]).dropna().sort_values('ratio')
 sv_ratio = sv_ratio.drop_duplicates(subset=['sample', 'svclass', 'value'])
 
-# sv_ratio = sv_ratio.assign(crispr=c_gdsc_fc.unstack().loc[[(s, g) for g, s in sv_ratio[['value', 'sample']].values]].values)
 # sv_ratio = sv_ratio.assign(crispy=c_gdsc.unstack().loc[[(s, g) for g, s in sv_ratio[['value', 'sample']].values]].values)
 
 
