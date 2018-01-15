d1e897108e59a25c8f482885373c757819fd3cbd
Emanuel Goncalves <eg14@sanger.ac.uk>
2018-01-15 16:05:32 +0000

    Roc analysis done in the SV

--- scripts/crispy/wgs_brass_cnv_ratio.py
+++ scripts/crispy/wgs_brass_cnv_ratio.py
@@ -13,13 +13,15 @@ from sklearn.metrics import roc_curve, auc
 
 # - Import
 # Brass
-brass_files = filter(lambda x: x.endswith('.brass.annot.bedpe'), os.listdir('data/gdsc/wgs/'))
-brass_df = pd.concat([pd.read_csv('data/gdsc/wgs/%s' % f, skiprows=110, sep='\t') for f in brass_files]).reset_index(drop=True)
-brass_df = brass_df.loc[list(map(lambda x: len(x.split(',')) == 1, brass_df['sample']))]
+brass_files = filter(lambda x: x.endswith('.brass.annot.bedpe'), os.listdir('data/gdsc/wgs/brass_bedpe/'))
+brass_df = pd.concat([pd.read_csv('data/gdsc/wgs/brass_bedpe/%s' % f, skiprows=110, sep='\t') for f in brass_files]).reset_index(drop=True)
+brass_df.to_csv('data/gdsc/wgs/wgs_brass_bedpe.csv')
 
 # Gene copy-number ratio
 cnv_ratios = pd.read_csv('data/crispy_copy_number_ratio.csv', index_col=0).replace(-1, np.nan)
-cnv_ratios.columns.name = 'sample'
+
+# Copy-number absolute counts
+cnv_abs = pd.read_csv('data/crispy_gene_copy_number.csv', index_col=0).replace(-1, np.nan)
 
 # GDSC CRISPR
 c_gdsc_fc = pd.read_csv('data/crispr_gdsc_logfc.csv', index_col=0)
@@ -39,16 +41,13 @@ print('Samples: ', len(samples), 'Genes: ', len(genes))
 
 
 # -
-brass_msv = brass_df.groupby(['sample', 'gene1'])['svclass'].agg(lambda x: len(set(x)))
-brass_msv = set(brass_msv[brass_msv > 1].index)
+sv_ratio = brass_df.loc[list(map(lambda x: len(x.split(',')) == 1, brass_df['sample']))]
 
-sv_ratio = pd.melt(brass_df.query("copynumber_flag == 1 & brass_score <= 100"), id_vars=['sample', 'svclass'], value_vars=['gene1', 'gene2']).query("value != '_'")
+sv_ratio = pd.melt(sv_ratio, id_vars=['sample', 'svclass'], value_vars=['gene1', 'gene2']).query("value != '_'")
 sv_ratio = sv_ratio[sv_ratio['value'].isin(genes) & sv_ratio['sample'].isin(samples)]
 
-sv_ratio = sv_ratio.assign(ratio=[cnv_ratios.loc[g, s] for g, s in sv_ratio[['value', 'sample']].values]).dropna()
+sv_ratio = sv_ratio.assign(ratio=[cnv_abs.loc[g, s] for g, s in sv_ratio[['value', 'sample']].values]).dropna().sort_values('ratio')
 sv_ratio = sv_ratio.drop_duplicates(subset=['sample', 'svclass', 'value'])
-
-sv_ratio = sv_ratio[[(s, g) not in brass_msv for s, g in sv_ratio[['sample', 'value']].values]]
 
 # sv_ratio = sv_ratio.assign(crispr=c_gdsc_fc.unstack().loc[[(s, g) for g, s in sv_ratio[['value', 'sample']].values]].values)
 # sv_ratio = sv_ratio.assign(crispy=c_gdsc.unstack().loc[[(s, g) for g, s in sv_ratio[['value', 'sample']].values]].values)
@@ -66,7 +65,7 @@ ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
 ax.set_xlabel('False positive rate')
 ax.set_ylabel('True positive rate')
-ax.set_title('Structural variation relation\nwith copy-number ratio')
+ax.set_title('Structural rearrangements relation with\ncopy-number ratio')
 
 legend = ax.legend(loc=4, prop={'size': 6})
 
