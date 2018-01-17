636e91ef9a1f0b4035c811cd70abdb30684ef277
Emanuel Goncalves <eg14@sanger.ac.uk>
2018-01-17 09:04:58 +0000

    Synthetic lethality analysis updated

--- scripts/synthetic_lethal_gexp.py
+++ scripts/synthetic_lethal_gexp.py
@@ -8,60 +8,59 @@ import pandas as pd
 import itertools as it
 import seaborn as sns
 import matplotlib.pyplot as plt
+from limix.plot import qqplot
 from limix.stats import qvalues
-from limix.qtl import qtl_test_lmm
 from scipy.optimize import curve_fit
 from matplotlib.gridspec import GridSpec
+from limix.qtl import qtl_test_lm, qtl_test_lmm
+from sklearn.preprocessing import StandardScaler
 from limix_core.util.preprocess import gaussianize
 from sklearn.metrics.regression import explained_variance_score
 
 
 # - Import data-sets
 # Essential genes
-essential = pd.read_csv('data/resources/curated_BAGEL_essential.csv', sep='\t')['gene'].rename('essential')
+essential = list(pd.read_csv('data/resources/curated_BAGEL_essential.csv', sep='\t')['gene'])
+
+# CRISPR
+crispr = pd.read_csv('data/gdsc/crispr/crisprcleanr_gene.csv', index_col=0).dropna()
+crispr = crispr.subtract(crispr.mean()).divide(crispr.std())
+
+# Gene-expression
+gexp = pd.read_csv('data/gdsc/gene_expression/merged_voom_preprocessed.csv', index_col=0)
+gexp = gexp.subtract(gexp.mean()).divide(gexp.std())
 
 # Samplesheet
 ss = pd.read_csv('data/gdsc/samplesheet.csv', index_col=0).dropna(subset=['Cancer Type'])
 
-# sgRNA library
-sglib = pd.read_csv('data/gdsc/crispr/KY_Library_v1.1_annotated.csv', index_col=0).dropna(subset=['GENES'])
 
-# CRISPR
-crispr = pd.read_csv('data/crispr_gdsc_crispy.csv', index_col=0)
-
-# Gene-expression
-gexp = pd.read_csv('data/gdsc/gene_expression/merged_voom_preprocessed.csv', index_col=0)
-
-# - Overlap
-samples = list(set(crispr).intersection(gexp))
+# - Filter + Overlap
+# Filter
+samples = list(set(crispr).intersection(gexp).intersection(ss.index))
 crispr, gexp = crispr[samples], gexp[samples]
-print(crispr.shape, gexp.shape)
 
-
-# - Filter
 # CRISPR
-crispr = crispr[(crispr.abs() >= 1).sum(1) >= 5]
-crispr = crispr[(crispr < -1).sum(1) < (crispr.shape[1] * .9)]
-crispr = crispr.drop(essential, axis=0, errors='ignore')
+crispr = crispr[(crispr < crispr.loc[essential].mean().mean()).sum(1) < (crispr.shape[1] * .5)]
+crispr = crispr[(crispr.abs() >= 2).sum(1) >= 5]
 
 # Gexp
-iqr_l, iqr_u = gexp.unstack().quantile(0.1), gexp.unstack().quantile(0.9)
-gexp = gexp.loc[((gexp < iqr_l) | (gexp > iqr_u)).sum(1) >= 10]
+gexp = gexp[(gexp < -1).sum(1) < (gexp.shape[1] * 0.5)]
+gexp = gexp[(gexp.abs() >= 1.5).sum(1) >= 5]
 print(crispr.shape, gexp.shape)
 
 
 # -
-# Covariates
-covars = pd.get_dummies(ss.loc[samples, ['Cancer Type', 'SCREEN MEDIUM', 'Microsatellite']])
-
 # Build matrices
-y, x, k = crispr[samples].T, gexp[samples].T, covars.dot(covars.T).astype(float)
+y, x = gexp[samples].T, crispr[samples].T
 
-# Normalise
-x = pd.DataFrame(gaussianize(x.values), index=x.index, columns=x.columns)
+# Standardize xs
+x = pd.DataFrame(StandardScaler().fit_transform(x), index=x.index, columns=x.columns)
+
+# Covariates
+covs = pd.get_dummies(ss.loc[samples, 'Cancer Type']).astype(float)
 
 # Linear regression
-lmm = qtl_test_lmm(pheno=y.values, snps=x.values, K=k.values)
+lmm = qtl_test_lm(pheno=y.values, snps=x.values, covs=covs.values)
 
 # Extract info
 c_gene, g_gene = zip(*list(it.product(y.columns, x.columns)))
@@ -74,8 +73,14 @@ lmm_df = pd.DataFrame({
     'pvalue': lmm.getPv().ravel(),
 })
 lmm_df = lmm_df[lmm_df['crispr'] != lmm_df['gexp']]
-lmm_df = lmm_df.assign(qvalue=qvalues(lmm_df['pvalue'].values))
-print(lmm_df.head(50))
+lmm_df = lmm_df.assign(qvalue=qvalues(lmm_df['pvalue'].values)).sort_values('qvalue')
+print(lmm_df.query('qvalue < 0.05'))
+
+
+qqplot(lmm.getPv().ravel())
+plt.show()
+
+lmm_df.query('qvalue < 0.05').to_csv('data/sl/gexp_crispr.csv', index=False)
 
 
 # - Plot
@@ -84,12 +89,10 @@ def fun(x, a, b, c):
     return a + b * x + c * (x ** 2)
 
 
-gpairs = [(x, y) for x, y in lmm_df.head(50)[['gexp', 'crispr']].values]
-
 sns.set(style='ticks', context='paper', font_scale=0.75, rc={'axes.linewidth': .3, 'xtick.major.width': .3, 'ytick.major.width': .3, 'xtick.major.size': 2.5, 'ytick.major.size': 2.5, 'xtick.direction': 'in', 'ytick.direction': 'in'})
 gs, pos = GridSpec(10, 5, hspace=.5, wspace=.5), 0
 # gx, gy = 'FAM50B', 'FAM50A'
-for gx, gy in gpairs:
+for beta, gx, gy, pval, qval in lmm_df.head(50).values:
     ax = plt.subplot(gs[pos])
 
     # Get measurements
@@ -114,10 +117,11 @@ for gx, gy in gpairs:
 
     ax.set_xlabel('%s gene-expression' % gx)
     ax.set_ylabel('%s CRISPR' % gy)
-    ax.set_title('y = {:.2f} + {:.2f} * x + {:.2f} * x**2'.format(*popt))
+    # ax.set_title('y = {:.2f} + {:.2f} * x + {:.2f} * x**2'.format(*popt))
+    ax.set_title('B=%.2f; FDR=%.2e' % (beta, qval))
 
     pos += 1
 
 plt.gcf().set_size_inches(2.5 * 5, 2.5 * 10)
-plt.savefig('reports/nlfit_scatter.png', bbox_inches='tight', dpi=600)
+plt.savefig('reports/sl/nlfit_scatter.png', bbox_inches='tight', dpi=600)
 plt.close('all')
