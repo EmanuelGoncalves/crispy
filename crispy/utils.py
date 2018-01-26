247d0eb8dfc64326d0b3cfb1479d56e4caaf6147
Emanuel Goncalves <eg14@sanger.ac.uk>
2018-01-26 12:50:29 +0000

    CNV bin function uptaded to handle nans

--- crispy/utils.py
+++ crispy/utils.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # Copyright (C) 2018 Emanuel Goncalves
 
+import numpy as np
 from sklearn.metrics import roc_auc_score
 
 
 def bin_cnv(value, thresold):
-    value = int(round(value, 0))
-    value_bin = '{}'.format(value) if value < thresold else '{}+'.format(thresold)
-    return value_bin
+    if np.isfinite(value):
+        value = int(round(value, 0))
+        return '{}'.format(value) if value < thresold else '{}+'.format(thresold)
+
+    else:
+        return np.nan
 
 
 def multilabel_roc_auc_score(y_true, y_pred, data, min_events=1, invert=-1):
