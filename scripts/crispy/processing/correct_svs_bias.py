442d19c042580ed4ca7a4ca63b34d59360eea74c
Emanuel Goncalves <eg14@sanger.ac.uk>
2018-02-13 10:24:58 +0000

    SVs correction with Crispy

---
+++ scripts/crispy/processing/correct_svs_bias.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# Copyright (C) 2018 Emanuel Goncalves
+
+import numpy as np
+import pandas as pd
+import itertools as it
+from crispy.utils import bin_cnv
+from crispy.biases_correction import CRISPRCorrection
+from scripts.crispy.plotting.annotate_bedpe import annotate_brass_bedpe
+
+
+SVTYPES = ['inversion', 'tandem-duplication', 'deletion']
+
+
+def count_svs(svs):
+    counts = dict(zip(*(SVTYPES, np.repeat(0, len(SVTYPES)))))
+    counts.update(dict(zip(*(np.unique(svs.split(','), return_counts=True)))))
+    return counts
+
+
+def present_svs(svs):
+    svs_set = set(svs.split(','))
+    return {sv.replace('-', ''): int(sv in svs_set) for sv in SVTYPES}
+
+
+def combinations_x(df, n, gate):
+    combs = [' {} '.format(gate).join(i) for i in it.permutations(list(df), n)]
+    return pd.DataFrame({i: df.eval(i) for i in combs})
+
+
+if __name__ == '__main__':
+    bedpe_dir = 'data/gdsc/wgs/brass_bedpe'
+    brca_samples = ['HCC1954', 'HCC1143', 'HCC38', 'HCC1187', 'HCC1937', 'HCC1395']
+
+    # Annotate BRASS bedpes
+    bed_dfs = annotate_brass_bedpe(bedpe_dir, bkdist=-1, splitreads=False, samples=brca_samples)
+
+    # - CRISPR
+    c_gdsc_fc = pd.read_csv('data/crispr_gdsc_logfc.csv', index_col=0).dropna()
+
+    # - CRISPR lib
+    sgrna_lib = pd.read_csv('data/crispr_libs/KY_Library_v1.1_updated.csv', index_col=0)
+
+    # - Copy-number
+    cnv = pd.read_csv('data/crispy_copy_number_gene_wgs.csv', index_col=0)
+
+    # - Overlap
+    genes = set(c_gdsc_fc.index).intersection(cnv.index)
+
+    # -
+    # sample = 'HCC1143'
+    crispy_samples = {}
+    for sample in brca_samples:
+        print('[INFO] {}'.format(sample))
+
+        svmatrix = pd.DataFrame(bed_dfs[bed_dfs['sample'] == sample].set_index('feature')['collapse'].apply(present_svs).to_dict()).T
+
+        svmatrix = svmatrix.reindex(genes).replace(np.nan, 0).astype(int)
+
+        svmatrix = pd.concat([svmatrix, combinations_x(svmatrix, 3, 'and')], axis=1)
+
+        svmatrix = svmatrix.assign(chr=sgrna_lib.groupby('gene')['chr'].first().reindex(svmatrix.index).values)
+
+        svmatrix = svmatrix.assign(crispr=c_gdsc_fc[sample].reindex(svmatrix.index).values)
+
+        svmatrix = svmatrix.assign(cnv=cnv[sample].reindex(svmatrix.index).values)
+
+        svmatrix = svmatrix.assign(cnv_bin=svmatrix['cnv'].apply(lambda v: bin_cnv(v, 10)).values)
+
+        svmatrix = svmatrix.dropna()
+
+        # Correction
+        crispy_cnv = CRISPRCorrection().rename(sample).fit_by(
+            by=svmatrix['chr'], X=svmatrix[['cnv']], y=svmatrix['crispr']
+        )
+        crispy_cnv = pd.concat([v.to_dataframe() for k, v in crispy_cnv.items()])
+        crispy_cnv.to_csv('data/crispy/gdsc_brass/{}.copynumber.csv'.format(sample))
+
+        # Correction
+        crispy_svs = CRISPRCorrection().rename(sample).fit_by(
+            by=svmatrix['chr'], X=svmatrix.drop(['chr', 'crispr'], axis=1), y=svmatrix['crispr']
+        )
+        crispy_svs = pd.concat([v.to_dataframe() for k, v in crispy_svs.items()])
+        crispy_cnv.to_csv('data/crispy/gdsc_brass/{}.svs.csv'.format(sample))
