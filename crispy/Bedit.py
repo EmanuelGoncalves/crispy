bb4bf86767cb5077bc1d236ed6805aa78690dea9
eg14 <emanuelvgoncalves@gmail.com>
2019-02-26 13:11:54 +0000

    Bedit module merged

---
+++ crispy/Bedit.py
@@ -0,0 +1,223 @@
+import re
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from crispy import Utils, CrispyPlot
+
+
+class Modifications:
+    AA = {
+        'A': dict(abbreviation='Ala', name='Alanine'),
+        'R': dict(abbreviation='Arg', name='Arginine'),
+        'N': dict(abbreviation='Asn', name='Asparagine'),
+        'D': dict(abbreviation='Asp', name='Aspartic acid'),
+        'C': dict(abbreviation='Cys', name='Cysteine'),
+        'Q': dict(abbreviation='Gln', name='Glutamine'),
+        'E': dict(abbreviation='Glu', name='Glutamic acid'),
+        'G': dict(abbreviation='Gly', name='Glycine'),
+        'H': dict(abbreviation='His', name='Histidine'),
+        'I': dict(abbreviation='Ile', name='Isoleucine'),
+        'M': dict(abbreviation='Met', name='Methionine'),
+        'L': dict(abbreviation='Leu', name='Leucine'),
+        'K': dict(abbreviation='Lys', name='Lysine'),
+        'F': dict(abbreviation='Phe', name='Phenylalanine'),
+        'P': dict(abbreviation='Pro', name='Proline'),
+        'S': dict(abbreviation='Ser', name='Serine'),
+        'T': dict(abbreviation='Thr', name='Threonine'),
+        'W': dict(abbreviation='Trp', name='Tryptophan'),
+        'Y': dict(abbreviation='Tyr', name='Tyrosine'),
+        'V': dict(abbreviation='Val', name='Valine'),
+        '*': dict(abbreviation='STOP', name='STOP')
+    }
+
+    @classmethod
+    def get_order(cls, field='abbreviation'):
+        order = cls.AA.keys()
+
+        if field is not None:
+            order = [cls.AA[i][field] for i in order]
+
+        return order
+
+    @classmethod
+    def get_names(cls, aas=None, field='abbreviation'):
+        if aas is None:
+            aas = cls.AA.keys()
+        return {aa: cls.AA[aa][field] for aa in aas if aa in cls.AA}
+
+
+class BaseEditor:
+    PAM_WT_CAS9 = 'NGG'
+
+    def __init__(self, edit_window=None, pam=None, base_edit=None, len_guide=None, name=None):
+        self.edit_window = edit_window
+        self.pam = pam
+        self.base_edit = base_edit
+        self.len_guide = len_guide
+        self.name = name
+
+    @staticmethod
+    def base_complement(nucleotide):
+        if nucleotide == 'C':
+            return 'G'
+        elif nucleotide == 'G':
+            return 'C'
+        elif nucleotide == 'A':
+            return 'T'
+        elif nucleotide == 'T':
+            return 'A'
+
+    @staticmethod
+    def parse_coordinates(genomic_coordinates):
+        chrm, pos = genomic_coordinates.split(':')
+        chrm, start, end = chrm, int(pos.split('-')[0]), int(pos.split('-')[1])
+        return chrm, start, end
+
+    def assert_guide(self, guide):
+        assert len(guide) == self.len_guide, f'Guide size: expected {self.len_guide} got {len(guide)}: {guide}'
+
+        pam_regex = self.pam.replace('N', '.')
+        assert re.search(f'{pam_regex}$', guide), f'PAM mismatch: expected {self.pam} got {guide[-len(self.pam):]}'
+
+    def split_guide(self, guide):
+        start = guide[:self.edit_window[0]]
+        edit = guide[self.edit_window[0]:self.edit_window[1]]
+        end = guide[self.edit_window[1]:-len(self.pam)]
+
+        pam = guide[-len(self.pam):]
+
+        return start, edit, end, pam
+
+    def print_guide(self, guide):
+        self.assert_guide(guide)
+
+        start, edit, end, pam = self.split_guide(guide)
+
+        print(f'{start} {edit} {end} [{pam}]')
+
+    def edit_guide(self, guide, guide_strand, target_strand):
+        start, edit, end, pam = self.split_guide(guide)
+
+        if guide_strand == target_strand:
+            edit = edit.replace(self.base_complement(self.base_edit[0]), self.base_complement(self.base_edit[1]))
+        else:
+            edit = edit.replace(self.base_edit[0], self.base_edit[1])
+
+        return start + edit + end + pam
+
+    def to_vep(self, guide_original, guide_edited):
+        start_win, original, end_win, pam = self.split_guide(guide_original)
+        _, edited, _, _ = self.split_guide(guide_edited)
+
+        if original == edited:
+            return None
+
+        else:
+            idx_start = min([i for i, bp in enumerate(original) if bp != edited[i]])
+            idx_end = len(original) - min([i for i, bp in enumerate(original[::-1]) if bp != edited[::-1][i]])
+
+            edit = f'{original[idx_start:idx_end]}/{edited[idx_start:idx_end]}'
+
+            return edit, len(start_win) + idx_start, len(start_win) + (idx_end - 1)
+
+    def list_base_edits(self, guide_original, guide_edited):
+        start_win, original, end_win, pam = self.split_guide(guide_original)
+        _, edited, _, _ = self.split_guide(guide_edited)
+
+        if original == edited:
+            return None
+
+        else:
+            return [(f'{original[i]}/{edited[i]}', len(start_win) + i) for i, bp in enumerate(original) if original[i] != edited[i]]
+
+
+class CytidineBaseEditor(BaseEditor):
+    def __init__(self, edit_window=(3, 8), pam=BaseEditor.PAM_WT_CAS9, len_guide=23, name='Cytidine base editor'):
+        super().__init__(
+            edit_window=edit_window,
+            pam=pam,
+            base_edit=('C', 'T'),
+            len_guide=len_guide,
+            name=name
+        )
+
+
+class AdenineBaseEditor(BaseEditor):
+    def __init__(self, edit_window=(3, 8), pam=BaseEditor.PAM_WT_CAS9, len_guide=23, name='Adenine base editor'):
+        super().__init__(
+            edit_window=edit_window,
+            pam=pam,
+            base_edit=('A', 'G'),
+            len_guide=len_guide,
+            name=name
+        )
+
+
+class BeditPlot(CrispyPlot):
+    # TODO: Test situtation where not all 20 aa and stop codon are present
+    @classmethod
+    def aa_grid(cls, dataframe=None, aggfunc=len, index_var='mutant', col_var='wildtype'):
+        order = Modifications.get_order(field='abbreviation')
+
+        plot_df = pd.pivot_table(
+            dataframe[[index_var, col_var]], index=index_var, columns=col_var, aggfunc=aggfunc,
+            fill_value=np.nan
+        ).loc[order, order]
+
+        #
+        f, axs = plt.subplots(
+            2, 2, sharex='col', sharey='row', gridspec_kw=dict(height_ratios=[1, 4], width_ratios=[4, 1])
+        )
+
+        g = sns.heatmap(
+            plot_df, cmap='Greys', annot=True, cbar=False, linewidths=.5, ax=axs[1, 0]
+        )
+
+        axs[1, 0].set_xlabel('Wild-type')
+        axs[1, 0].set_ylabel('Mutant')
+        axs[1, 0].grid(lw=.5, ls='--', color=cls.PAL_DBGD[2])
+        sns.despine(right=False, top=False, ax=axs[1, 0])
+
+        for i in [0, 1]:
+            i_sums = plot_df.sum(i)
+
+            norm = mpl.colors.Normalize(vmin=0, vmax=i_sums.max())
+            colors = [plt.cm.Greys(norm(v)) for v in i_sums]
+
+            barfun = axs[i, i].barh if i else axs[i, i].bar
+            barfun(np.arange(len(order)) + .5, i_sums, color=colors)
+
+            axs[i, i].set_xlabel('Count' if i else '')
+            axs[i, i].set_ylabel('' if i else 'Count')
+            sns.despine(ax=axs[i, i])
+
+        axs[0, 1].axis('off')
+
+        plt.subplots_adjust(hspace=0, wspace=0)
+
+        return g
+
+    @classmethod
+    def aa_countplot(cls, dataframe, index_var='mutant', col_var='wildtype'):
+        plot_df = pd.concat([
+            dataframe[index_var].value_counts().rename('Mutant'),
+            dataframe[col_var].value_counts().rename('Wild-type')
+        ], axis=1, sort=False)
+
+        plot_df = plot_df.unstack().reset_index()
+        plot_df.columns = ['type', 'aa', 'count']
+
+        order = Modifications.get_order()
+
+        pal = {'Mutant': cls.PAL_DBGD[0], 'Wild-type': cls.PAL_DBGD[2]}
+
+        sns.barplot('count', 'aa', 'type', data=plot_df, palette=pal, order=order, orient='h')
+
+        plt.grid(lw=.3, ls='-', color=cls.PAL_DBGD[2], axis='x', zorder=0)
+
+        plt.legend(frameon=False, prop=dict(size=4))
+
+        plt.xlabel('AA count')
+        plt.ylabel('')
---
+++ crispy/CircularBinarySegmentation.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# Copyright (C) 2019 Emanuel Goncalves
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from CrispyPlot import MidpointNormalize
+from crispy.Bedit import CrispyPlot
+
+
+class CBS:
+    @classmethod
+    def run(cls, data, shuffles=int(1e3), p=0.01):
+        segments = cls.segment(data, shuffles=shuffles, p=p)
+        segments = cls.validate(data, segments, shuffles=shuffles, p=p)
+        return segments
+
+    @staticmethod
+    def cbs_stat(x):
+        """
+        Given x, Compute the subinterval x[i0:i1] with the maximal segmentation statistic t.
+        Returns t, i0, i1
+
+        :param x:
+        :return:
+        """
+
+        x0 = x - np.mean(x)
+        n = len(x0)
+        y = np.cumsum(x0)
+        e0, e1 = np.argmin(y), np.argmax(y)
+        i0, i1 = min(e0, e1), max(e0, e1)
+        s0, s1 = y[i0], y[i1]
+        return (s1 - s0) ** 2 * n / (i1 - i0 + 1) / (n + 1 - i1 + i0), i0, i1 + 1
+
+    @staticmethod
+    def tstat(x, i):
+        """
+        Return the segmentation statistic t testing if i is a (one-sided)  breakpoint in x
+
+        :param x:
+        :param i:
+        :return:
+        """
+        n = len(x)
+        s0 = np.mean(x[:i])
+        s1 = np.mean(x[i:])
+        return (n - i) * i / n * (s0 - s1) ** 2
+
+    @classmethod
+    def cbs(cls, x, shuffles=1000, p=.05):
+        """
+        Given x, find the interval x[i0:i1] with maximal segmentation statistic t. Test that statistic against
+        given (shuffles) number of random permutations with significance p.  Return True/False, t, i0, i1; True if
+        interval is significant, false otherwise.
+
+        :param x:
+        :param shuffles:
+        :param p:
+        :return:
+        """
+
+        max_t, max_start, max_end = cls.cbs_stat(x)
+        if max_end - max_start == len(x):
+            return False, max_t, max_start, max_end
+        if max_start < 5:
+            max_start = 0
+        if len(x) - max_end < 5:
+            max_end = len(x)
+        thresh_count = 0
+        alpha = shuffles * p
+        xt = x.copy()
+        for i in range(shuffles):
+            np.random.shuffle(xt)
+            threshold, s0, e0 = cls.cbs_stat(xt)
+            if threshold >= max_t:
+                thresh_count += 1
+            if thresh_count > alpha:
+                return False, max_t, max_start, max_end
+        return True, max_t, max_start, max_end
+
+    @classmethod
+    def rsegment(cls, x, start, end, segments=None, shuffles=1000, p=.05):
+        """
+        Recursively segment the interval x[start:end] returning a list L of pairs (i,j) where each (i,j) is a
+        significant segment.
+
+        :param x:
+        :param start:
+        :param end:
+        :param segments:
+        :param shuffles:
+        :param p:
+        :return:
+        """
+
+        if segments is None:
+            segments = []
+
+        threshold, t, s, e = cls.cbs(x[start:end], shuffles=shuffles, p=p)
+
+        if (not threshold) | (e - s < 5) | (e - s == end - start):
+            segments.append((start, end))
+
+        else:
+            if s > 0:
+                cls.rsegment(x, start, start + s, segments)
+
+            if e - s > 0:
+                cls.rsegment(x, start + s, start + e, segments)
+
+            if start + e < end:
+                cls.rsegment(x, start + e, end, segments)
+
+        return segments
+
+    @classmethod
+    def segment(cls, data, shuffles, p):
+        """
+        Segment the array x, using significance test based on shuffles rearrangements and significance level p
+        :param x:
+        :param shuffles:
+        :param p:
+        :return:
+        """
+
+        if type(data) == pd.Series:
+            data = data.values
+
+        start, end = 0, len(data)
+
+        segments = []
+
+        return cls.rsegment(data, start, end, segments, shuffles=shuffles, p=p)
+
+    @classmethod
+    def validate(cls, x, L, shuffles, p):
+        if type(x) == pd.Series:
+            x = x.values
+
+        S = [x[0] for x in L] + [len(x)]
+        SV = [0]
+        left = 0
+        for test, s in enumerate(S[1:-1]):
+            t = cls.tstat(x[S[left]:S[test + 2]], S[test + 1] - S[left])
+
+            threshold = 0
+            thresh_count = 0
+            site = S[test + 1] - S[left]
+            xt = x[S[left]:S[test + 2]].copy()
+            flag = True
+
+            for k in range(shuffles):
+                np.random.shuffle(xt)
+                threshold = cls.tstat(xt, site)
+
+                if threshold > t:
+                    thresh_count += 1
+
+                if thresh_count >= p * shuffles:
+                    flag = False
+                    break
+
+            if flag:
+                SV.append(S[test + 1])
+                left += 1
+
+        SV.append(S[-1])
+
+        return SV
+
+    @staticmethod
+    def generate_normal_time_series(num, minl=50, maxl=1000):
+        """
+        Generate a time series with num segments of minimal length minl and maximal length maxl.  Within a segment,
+        data is normal with randomly chosen, normally distributed mean between -10 and 10, variance between 0 and 1.
+
+        :param num:
+        :param minl:
+        :param maxl:
+        :return:
+        """
+
+        data = np.array([], dtype=np.float64)
+        partition = np.random.randint(minl, maxl, num)
+        for p in partition:
+            mean = np.random.randn() * 10
+            var = np.random.randn() * 1
+            if var < 0:
+                var = var * -1
+            tdata = np.random.normal(mean, var, p)
+            data = np.concatenate((data, tdata))
+        return data
+
+    @staticmethod
+    def draw_segmented_data(data, segments, ax=None):
+        """
+        Draw a scatterplot of the data with vertical lines at segment boundaries and horizontal lines at means of
+        the segments. S is a list of segment boundaries.
+
+        :param data:
+        :param S:
+        :param ax:
+        :return:
+        """
+
+        if ax is None:
+            ax = plt.gca()
+
+        ax.scatter(range(len(data)), data.values, s=1, c=data, cmap='RdYlBu', norm=MidpointNormalize(midpoint=0.))
+
+        ax.axhline(0, ls='-', lw=.3, c=CrispyPlot.PAL_DBGD[2], zorder=0)
+
+        for x in segments:
+            ax.axvline(x, ls='--', lw=.3, c=CrispyPlot.PAL_DBGD[2], zorder=0)
+
+        for i in range(1, len(segments)):
+            y = np.mean(data.values[segments[i - 1]:segments[i]])
+            ax.hlines(y, segments[i - 1], segments[i], color=CrispyPlot.PAL_DBGD[1], lw=1.)
+
+        ax.set_title(data.name)
+
+        return ax
